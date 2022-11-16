e4c5aa440e49275b367d51bcc65a8b0a51a5b11a
iamdudeman <timothy.solum@gmail.com>
2022-11-15 21:06:42 -0600

    Added task to swing and javafx for distributable fat jar creation

--- javafx/build.gradle.kts
+++ javafx/build.gradle.kts
@@ -1,10 +1,5 @@
 plugins {
-  id("application")
   id("sola.java-conventions")
-}
-
-application {
-  mainClass.set("${project.properties["basePackage"]}.javafx.JavaFxMain")
 }
 
 dependencies {
@@ -17,6 +12,36 @@ dependencies {
   runtimeOnly("org.openjfx", "javafx-controls", "17.0.2", classifier = osClassifier)
   runtimeOnly("org.openjfx", "javafx-graphics", "17.0.2", classifier = osClassifier)
 }
+
+tasks.withType<Zip> {
+  duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+}
+
+tasks.withType<Tar> {
+  duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+}
+
+task("distFatJar", Jar::class) {
+  group = "distribution"
+  duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+
+  val osClassifier = getOsClassifier()
+  archiveBaseName.set("${project.properties["gameName"]}-${project.name}-${osClassifier}")
+
+  manifest {
+    attributes["Main-Class"] = "${project.properties["basePackage"]}.javafx.JavaFxMain"
+  }
+
+  val dependencies = configurations.runtimeClasspath.get().map(::zipTree)
+
+  from(dependencies)
+  from("${project.rootDir}/assets") {
+    into("assets")
+  }
+  with(tasks.jar.get())
+  dependsOn(configurations.runtimeClasspath)
+}
+
 
 fun getOsClassifier(): String {
   if (org.apache.tools.ant.taskdefs.condition.Os.isFamily(org.apache.tools.ant.taskdefs.condition.Os.FAMILY_MAC)) {
