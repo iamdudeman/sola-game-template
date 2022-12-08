1690acfe75d214c3ee72cc7958c6e986e8637604
iamdudeman <timothy.solum@gmail.com>
2022-12-07 21:50:13 -0600

    Updated sola version

--- game/src/main/java/technology/sola/engine/game/GameSola.java
+++ game/src/main/java/technology/sola/engine/game/GameSola.java
@@ -13,12 +13,11 @@ import technology.sola.engine.graphics.Color;
 import technology.sola.engine.graphics.components.RectangleRendererComponent;
 import technology.sola.engine.graphics.components.SpriteComponent;
 import technology.sola.engine.graphics.renderer.Renderer;
+import technology.sola.engine.graphics.screen.AspectMode;
 import technology.sola.engine.input.Key;
 import technology.sola.engine.physics.Material;
 import technology.sola.engine.physics.component.ColliderComponent;
 import technology.sola.engine.physics.component.DynamicBodyComponent;
-
-import java.io.Serial;
 
 public class GameSola extends Sola {
   private SolaGraphics solaGraphics;
@@ -36,6 +35,8 @@ public class GameSola extends Sola {
     assetLoaderProvider.get(SpriteSheet.class)
       .addAssetMapping("test", "assets/test_tiles_spritesheet.json");
 
+    platform.getViewport().setAspectMode(AspectMode.MAINTAIN);
+
     solaEcs.addSystems(new PlayerSystem());
     solaEcs.setWorld(buildWorld());
   }
@@ -52,7 +53,7 @@ public class GameSola extends Sola {
 
     world.createEntity()
       .addComponent(new PlayerComponent())
-      .addComponent(new TransformComponent(200, 300))
+      .addComponent(new TransformComponent(200, 300, 1, 1))
       .addComponent(new SpriteComponent("test", "blue"))
       .addComponent(ColliderComponent.aabb(16, 16))
       .addComponent(new DynamicBodyComponent(new Material(1, 0.1f, 50)))
@@ -67,8 +68,6 @@ public class GameSola extends Sola {
   }
 
   private record PlayerComponent() implements Component {
-    @Serial
-    private static final long serialVersionUID = -8026881223157823822L;
   }
 
   private class PlayerSystem extends EcsSystem {
@@ -90,7 +89,7 @@ public class GameSola extends Sola {
           if (dynamicBodyComponent.isGrounded() && keyboardInput.isKeyHeld(Key.SPACE)) {
             dynamicBodyComponent.applyForce(0, -2500);
           } else if (dynamicBodyComponent.getVelocity().y() > 0) {
-            dynamicBodyComponent.applyForce(0, 1.2f * solaPhysics.getGravitySystem().getGravityConstant() * dynamicBodyComponent.getMaterial().getMass());
+            dynamicBodyComponent.applyForce(0, 2f * solaPhysics.getGravitySystem().getGravityConstant() * dynamicBodyComponent.getMaterial().getMass());
           }
         });
     }
