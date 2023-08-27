6947a85e9d6d7ed30a388ecb648ca9e9b29ab29d
iamdudeman <timothy.solum@gmail.com>
2023-08-27 16:45:51 -0500

    Using SolaWithDefaults now and GenerateBrowserFilesMain defaults to with overlay

--- swing/src/main/java/technology/sola/swing/SwingMain.java
+++ swing/src/main/java/technology/sola/swing/SwingMain.java
@@ -2,13 +2,13 @@ package technology.sola.swing;
 
 import technology.sola.engine.core.SolaPlatform;
 import technology.sola.engine.core.Sola;
-import technology.sola.engine.game.GameSola;
+import technology.sola.engine.game.GameSolaWithDefaults;
 import technology.sola.engine.platform.swing.SwingSolaPlatform;
 
 public class SwingMain {
   public static void main(String[] args) {
     SolaPlatform solaPlatform = new SwingSolaPlatform();
-    Sola sola = new GameSola();
+    Sola sola = new GameSolaWithDefaults();
 
     solaPlatform.play(sola);
   }
