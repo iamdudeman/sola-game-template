77f6a5f68838d533d1ea321e6eee6acefcf52345
iamdudeman <timothy.solum@gmail.com>
2025-05-15 17:46:57 -0500

    updated engine version configured new logging code updated path to spritesheet asset

--- swing/src/main/java/technology/sola/swing/SwingMain.java
+++ swing/src/main/java/technology/sola/swing/SwingMain.java
@@ -4,8 +4,15 @@ import technology.sola.engine.core.SolaPlatform;
 import technology.sola.engine.core.Sola;
 import technology.sola.engine.game.GameSola;
 import technology.sola.engine.platform.swing.SwingSolaPlatform;
+import technology.sola.logging.JavaSolaLoggerFactory;
+import technology.sola.logging.SolaLogLevel;
+import technology.sola.logging.SolaLogger;
 
 public class SwingMain {
+  static {
+    SolaLogger.configure(SolaLogLevel.WARNING, new JavaSolaLoggerFactory());
+  }
+
   public static void main(String[] args) {
     SolaPlatform solaPlatform = new SwingSolaPlatform();
     Sola sola = new GameSola();
