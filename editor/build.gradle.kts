2773337bd62081c6d5ec8c067dd3230d70c0917e
iamdudeman <timothy.solum@gmail.com>
2025-06-20 09:41:01 -0500

    cleanup

--- editor/build.gradle.kts
+++ editor/build.gradle.kts
@@ -1,15 +1,11 @@
 import technology.sola.plugins.SolaJavaDistributionPluginExtension
 
-plugins {
-  id("java")
-}
-
 apply(plugin = "technology.sola.plugins.sola-java-conventions")
 apply(plugin = "technology.sola.plugins.sola-java-distribution")
 
-dependencies {
-  implementation("com.github.iamdudeman.sola-game-engine:editor:${project.properties["solaVersion"]}")
-  implementation(project(":game"))
+configure<JavaPluginExtension> {
+  dependencies.add("implementation", "com.github.iamdudeman.sola-game-engine:editor:${project.properties["solaVersion"]}")
+  dependencies.add("implementation", project(":game"))
 }
 
 configure<SolaJavaDistributionPluginExtension> {
--- game/build.gradle.kts
+++ game/build.gradle.kts
@@ -1,9 +1,5 @@
-plugins {
-  id("java")
-}
-
 apply(plugin = "technology.sola.plugins.sola-java-conventions")
 
-dependencies {
-  implementation("com.github.iamdudeman.sola-game-engine:sola-engine:${project.properties["solaVersion"]}")
+configure<JavaPluginExtension> {
+  dependencies.add("implementation", "com.github.iamdudeman.sola-game-engine:sola-engine:${project.properties["solaVersion"]}")
 }
