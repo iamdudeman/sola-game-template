b7955e2ebec1840507f01fb8204de222f911f5e4
iamdudeman <timothy.solum@gmail.com>
2025-06-20 21:30:38 -0500

    updated snapshot version of engine

--- android/src/main/java/technology/sola/android/MainActivity.java
+++ android/src/main/java/technology/sola/android/MainActivity.java
@@ -3,14 +3,18 @@ package technology.sola.android;
 import technology.sola.engine.core.Sola;
 import technology.sola.engine.game.GameSola;
 import technology.sola.engine.platform.android.SolaAndroidActivity;
-import technology.sola.logging.JavaSolaLoggerFactory;
+import technology.sola.engine.platform.android.core.AndroidSolaLoggerFactory;
+import technology.sola.engine.platform.android.core.SolaAndroidPlatformConfig;
 import technology.sola.logging.SolaLogLevel;
 import technology.sola.logging.SolaLogger;
 
 public class MainActivity extends SolaAndroidActivity {
   static {
-    // todo might need AndroidSolaLoggerFactory
-    SolaLogger.configure(SolaLogLevel.WARNING, new JavaSolaLoggerFactory());
+    SolaLogger.configure(SolaLogLevel.WARNING, new AndroidSolaLoggerFactory());
+  }
+
+  public MainActivity() {
+    super(new SolaAndroidPlatformConfig());
   }
 
   @Override
