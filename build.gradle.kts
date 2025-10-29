2ce8ee2452683cfb1b0a47b5c6bd33b7a82b8f42
iamdudeman <timothy.solum@gmail.com>
2025-10-29 16:45:35 -0500

    updated gradle wrapper

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ subprojects {
     }
 
     dependencies {
-      classpath("com.android.tools.build:gradle:8.11.1")
+      classpath("com.android.tools.build:gradle:8.12.3")
 
       classpath("com.github.iamdudeman.sola-game-engine:technology.sola.plugins.sola-android-conventions.gradle.plugin:${project.properties["solaVersion"]}")
       classpath("com.github.iamdudeman.sola-game-engine:technology.sola.plugins.sola-android-app.gradle.plugin:${project.properties["solaVersion"]}")
